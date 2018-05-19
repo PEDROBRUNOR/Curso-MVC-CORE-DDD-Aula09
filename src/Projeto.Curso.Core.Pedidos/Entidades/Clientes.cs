73ab99684fec9c42fc4c7000f96034d3b9d8f051
Pedro Bruno <pedrobrunor@hotmail.com>
2018-05-19 19:17:38 -0300

    aula09

---
+++ src/Projeto.Curso.Core.Pedidos/Entidades/Clientes.cs
@@ -0,0 +1,32 @@
+﻿using Projeto.Curso.Core.Domain.Shared.Entidades;
+using System.Linq;
+
+namespace Projeto.Curso.Core.Domain.Pedidos.Entidades
+{
+    public class Clientes : Pessoa
+    {
+        public override bool EstaConsistente()
+        {
+            ApelidoDeveSerPreenchido();
+            ApelidoDeveTerUmTamanhoLimite(20);
+            NomeDeveSerPreenchido();
+            NomeDeveTerUmTamanhoLimite(100);
+            CPFouCNPJDeveSerPreenchido();
+            CPFouCNPJDeveSerValido();
+            EmaiDeveSerValido();
+            EmailDeveTerUmTamanhoLimite(100);
+            EnderecoDeveSerPreenchido();
+            EnderecoDeveTerUmTamanhoLimite(100);
+            BairroDeveTerUmTamanhoLimite(30);
+            CidadeDeveSerPreenchida();
+            CidadeDeveTerUmTamanhoLimite(30);
+            UFDeveSerPreenchida();
+            UFDeveSerValida();
+            CepDeveSerValido();
+            return !ListaErros.Any();
+        }
+
+
+
+    }
+}
---
+++ src/Projeto.Curso.Core.Pedidos/Entidades/Fornecedores.cs
@@ -0,0 +1,29 @@
+﻿using Projeto.Curso.Core.Domain.Shared.Entidades;
+using System.Linq;
+
+namespace Projeto.Curso.Core.Domain.Pedidos.Entidades
+{
+    public class Fornecedores : Pessoa
+    {
+        public override bool EstaConsistente()
+        {
+            ApelidoDeveSerPreenchido();
+            ApelidoDeveTerUmTamanhoLimite(20);
+            NomeDeveSerPreenchido();
+            NomeDeveTerUmTamanhoLimite(100);
+            CPFouCNPJDeveSerPreenchido();
+            CPFouCNPJDeveSerValido();
+            EmaiDeveSerValido();
+            EmailDeveTerUmTamanhoLimite(100);
+            EnderecoDeveSerPreenchido();
+            EnderecoDeveTerUmTamanhoLimite(100);
+            BairroDeveTerUmTamanhoLimite(30);
+            CidadeDeveSerPreenchida();
+            CidadeDeveTerUmTamanhoLimite(30);
+            UFDeveSerPreenchida();
+            UFDeveSerValida();
+            CepDeveSerValido();
+            return !ListaErros.Any();
+        }
+    }
+}
